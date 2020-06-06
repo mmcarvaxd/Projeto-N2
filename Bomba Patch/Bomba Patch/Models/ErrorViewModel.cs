77d321c4fed42059c9ca5569dd732549b21748cb
Matheus Carvalho <matheuscarva@hotmail.com>
2020-06-05 23:44:34 -0300

    Initial commit

---
+++ Bomba Patch/Bomba Patch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bomba_Patch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

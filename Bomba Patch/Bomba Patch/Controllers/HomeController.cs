25efe3268f69d0ea9c88bf2c13a6e0dca1b6bd55
Matheus Carvalho <matheuscarva@hotmail.com>
2020-06-06 22:06:49 -0300

    criado tela de login

--- Bomba Patch/Bomba Patch/Controllers/HomeController.cs
+++ Bomba Patch/Bomba Patch/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Bomba_Patch.Models;
 
 namespace Bomba_Patch.Controllers
 {
@@ -17,27 +16,7 @@ namespace Bomba_Patch.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
